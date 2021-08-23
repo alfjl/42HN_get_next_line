51f5c4bf1f710f294e413ddfc81d200b26a36180
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2021-08-23 14:12:35 +0200

    Add outline of main block to .c file

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alanghan <alanghan@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/21 17:33:56 by alanghan          #+#    #+#             */
-/*   Updated: 2021/08/23 11:58:33 by alanghan         ###   ########.fr       */
+/*   Updated: 2021/08/23 13:57:26 by alanghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,22 +30,26 @@ char	*get_next_line(int fd)
 	// while ((read_bytes = read(fd, buf, BUFFER_SIZE)) > 0)	// if read() does not return 0 for EOF, or -1 for error  --> NORMINETTE ERROR: Assignment in controle structure
 	while (read_bytes >= 0) // if read() does not return -1 for error (if return 0 for EOF, we still have to process the last read bytes)
 		{
-			// malloc size for first buffer (but if no '\n' in BUFFER-SIZE, we keep on reading, so we need new malloced buff mor often!)
+			// malloc size for first buffer (but if no '\n' in BUFFER-SIZE, we keep on reading, so we need new malloced buff more often!)
 			buf = (char *)malloc(((BUFFER_SIZE + 1) * sizeof(char)));
 			if (buf == NULL)
 				return (NULL);
 			// read chars BUFFER_SIZE into read_bytes
 			read_bytes = read(fd, buf, BUFFER_SIZE);
+			if (read_bytes < 0)
+			{
+				free(buf);
+				return (NULL);
+			}
+			ft_readjoin(&stat_buffer, &buf);
 			// if theres already a '\n', cut stat_buffer down and return (see below). QUESTION: What to do with read_bytes? Do we need that for next round to step in correctly?
 			if (ft_strchr(buf, '\n') != NULL)
-				ft_cut_line(stat_buffer, buf) // di I need to include 'read_bytes' here, to shorten to correct length, for later run?   |   to make code shorter, use 'return (ft_cut_line(...))'. But first try to only use one return value at end of function!
-			// if theres no '\n'?
-			
+				ft_cut_line(&stat_buffer, &buf, &read_bytes); // do I need to include 'read_bytes' here, to shorten to correct length, for later run?   |   to make code shorter, use 'return (ft_cut_line(...))'. But first try to only use one return value at end of function!
 			// if there's EOF (read_bytes == 0)
-			
+			else if (read_bytes == 0)
+				ft_cut_last_line(&stat_buffer, &buf);
 		}
-
 	// ---------- End Pattern (free & return) --------
 	// free something!
-	return (stat_buffer);
+	return (&stat_buffer);
 }
