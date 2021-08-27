371c3feabfeb3c145336181e32f66c0bd2e1ec04
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2021-08-27 13:40:19 +0200

    Add function prototypes to get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alanghan <alanghan@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/21 17:33:56 by alanghan          #+#    #+#             */
-/*   Updated: 2021/08/27 13:29:56 by alanghan         ###   ########.fr       */
+/*   Updated: 2021/08/27 13:38:50 by alanghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,14 @@ char	*get_next_line(int fd)
 	int			bytes_read;
 	char		*buf;
 	char		*line;
+	t_string	string;
 
 	// ---------- Guard Pattern ----------------------
 	if (fd < 0)
 		return (NULL);
 
 	// ---------- Variable Initialization Pattern ----
-	string_create();
+	string_create(&string);
 	bytes_read = 0;
 
 	// ---------- Main Body --------------------------
@@ -35,7 +36,7 @@ char	*get_next_line(int fd)
 		i = 0;
 		buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 		bytes_read = read(fd, buf, BUFFER_SIZE);
-		string_correct_chars();
+		string_correct_chars(&string, buf, &i);
 		if (buf[i] == '\n')
 		{
 			string_append_chars('\n');
@@ -45,15 +46,20 @@ char	*get_next_line(int fd)
 		}
 		free(buf);
 	}
-	line = string_as_c_string();
+	line = string_as_c_string(&string);
 	// ---------- End Pattern (free & return) --------
-	string_destroy();
+	string_destroy(&string);
 	return (line);
 }
 /* ----------------------------- FUNC 2 ------------------------------------- */
-
+	string_create(&string);
 /* ----------------------------- FUNC 3 ------------------------------------- */
-
+string_correct_chars(&string, buf, &i)
 /* ----------------------------- FUNC 4 ------------------------------------- */
-
+string_append_chars('\n')
 /* ----------------------------- FUNC 5 ------------------------------------- */
+string_as_c_string(&string)
+/* ----------------------------- FUNC 6 ------------------------------------- */
+string_destroy(&string)
+/* ----------------------------- FUNC 6 ------------------------------------- */
+string_append_chars()
