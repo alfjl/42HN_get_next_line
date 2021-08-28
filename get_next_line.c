82152c433b42725f286cb7f47980a7be124b6c05
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2021-08-28 10:17:51 +0200

    Before v.2.1

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alanghan <alanghan@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/21 17:33:56 by alanghan          #+#    #+#             */
-/*   Updated: 2021/08/27 18:24:09 by alanghan         ###   ########.fr       */
+/*   Updated: 2021/08/28 10:15:57 by alanghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,12 +40,16 @@ char	*get_next_line(int fd)
 			return (NULL);
 		}
 		bytes_read = read(fd, buf, BUFFER_SIZE);
-		printf("I'm here!!!!");
+		//string_correct_chars(&string, buf, &i, &bytes_read);
+		printf("\ni VOR = %i\n", i); // ################################################### TPO #############################################
 		string_correct_chars(&string, buf, &i);
+		printf("i NACH = %i\n", i); // ################################################### TPO #############################################
 		if (buf[i] == '\n')
 		{
+			// string_append_chars(&string, '\n', &bytes_read);
 			string_append_chars(&string, '\n');
 			i++;
+			//string_append_chars(&string, '\0');
 			free(buf); // maybe not needed, if 'break' only breaks from 'if', not 'while'. Then another break point needed in 'while'!
 			break;
 		}
@@ -72,7 +76,14 @@ void	string_correct_chars(t_string *string, char *buf, int *i)
 	while (*i < BUFFER_SIZE)
 	{
 		if (buf[*i] != '\n' && buf[*i] != EOF)
+		{
 			string_append_chars(string, buf[*i]);
+			*i += 1;
+			//string_append_chars(string, '\0');
+			printf("i IN = %i\n", *i); // ################################################### TPO #############################################
+		}
+		else
+			break;
 	}
 }
 
@@ -82,9 +93,10 @@ void	string_append_chars(t_string *string, char c)
 	char	*temp;
 	int		j;
 
+	//printf("\nHERE!!!!\n"); // ################################################### TPO #############################################
 	if (string->filled >= string->allocated)
 	{
-		temp = (char *)malloc(sizeof(char) * (string->filled + BUFFER_SIZE));
+		temp = (char *)malloc(sizeof(char) * (string->filled + BUFFER_SIZE + 1));
 		if (temp == NULL)
 		{
 			string_destroy(string);
@@ -98,7 +110,7 @@ void	string_append_chars(t_string *string, char c)
 				temp[j] = string->chars[j];
 				j++;
 			}
-			//free(string->chars);
+			//free(string->chars);	// Why does this give malloc error (maybe 'double free' error)?
 			string->chars = temp;
 			string->allocated = string->filled + BUFFER_SIZE;
 		}
