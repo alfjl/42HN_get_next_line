f6f465c21f0169ba4c9020b4f0fbda0a403febe7
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2021-09-02 11:36:26 +0200

    Solved issue with last line. Only still solve issue with NULL return if file empty

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alanghan <alanghan@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/21 17:33:56 by alanghan          #+#    #+#             */
-/*   Updated: 2021/09/01 11:56:10 by alanghan         ###   ########.fr       */
+/*   Updated: 2021/09/02 11:35:14 by alanghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,44 @@
 char	*get_next_line(int fd)
 {
 	// ---------- Variable Declaration Pattern -------
-	int				i;
+	int				newly_created;
+	int				malloc_error;
 	int				bytes_read;
 	static t_buffer	buffer;
 	t_line			line;
 
 	// ---------- Guard Pattern ----------------------
-	if (fd < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	//printf("READ_HEAD BEGINNING = %d\n", buffer.read_head); // ######################## TPO #######################
 
 	// ---------- Variable Initialization Pattern ----
-	buffer_create(&buffer);
-	line_create(&line);
+	bytes_read = 1;
+	newly_created = __OFF__;
+	buffer_create(&buffer, &newly_created);
+	malloc_error = line_create(&line);
+	if (malloc_error == 0)
+		return (NULL);
 	// ---------- Main Body --------------------------
 	while (bytes_read >= 0)
 	{
-		buffer_write(&buffer, &bytes_read, fd);
-		line_write(&line, &buffer, &i);
+		buffer_write(&buffer, &bytes_read, fd, &newly_created);
+			//printf("line = %s\n", line.chars); // ####### TPO ############
+		line_write(&line, &buffer);
 		if (line.chars[line.filled - 1] == '\n')
 			break;
-		else if (bytes_read == 0 && buffer.chars[buffer.read_head] == '\0')
+		if (bytes_read == 0 && buffer.chars[buffer.read_head] == '\0')
+		{
+			//printf("buffer.chars[buffer.read_head] = %d\n", buffer.chars[buffer.read_head]); // ########## TPO #############
+			//printf("buffer.chars[buffer.read_head - 1] = %d\n", buffer.chars[buffer.read_head - 1]); // ########## TPO #############
+			// printf("bytes_read = %d\n", bytes_read); // ########## TPO #############
 			break;
+		}
 	}
 	//buffer_destroy(&buffer);
 	return (line.chars);
 }
 /* ----------------------------- FUNC 2 ------------------------------------- */
-void	buffer_create(t_buffer *buffer)
+void	buffer_create(t_buffer *buffer, int *newly_created)
 {
 	// if (buffer->chars == NULL)
 	if (!buffer->chars[0]) // change to correct test! Don't use "!".
@@ -51,20 +61,19 @@ void	buffer_create(t_buffer *buffer)
 		ft_bzero(buffer->chars, BUFFER_SIZE);
 		buffer->write_head = BUFFER_SIZE;
 		buffer->read_head = 0;
+		*newly_created = __ON__;
 	}
 }
 /* ----------------------------- FUNC 3 ------------------------------------- */
-void	line_create(t_line *line)
+int	line_create(t_line *line)
 {
 	line->chars = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (line->chars == NULL)
-	{
-		//buffer_destroy(&buffer); // muss noch implementiert werden!!!!!! Eigentlich nicht, da BUFFER nicht gemalloced!!!
-		//return (NULL); // muss noch abgefangen werden!!!!!!
-	}
+		return (0);
 	ft_bzero(line->chars, BUFFER_SIZE + 1);
 	line->allocated = BUFFER_SIZE;
 	line->filled = 0;
+	return (1);
 }
 /* ----------------------------- FUNC 4 ------------------------------------- */
 void	buffer_destroy(t_buffer *buffer)
@@ -76,19 +85,25 @@ void	buffer_destroy(t_buffer *buffer)
 	buffer->read_head = 0;
 }
 /* ----------------------------- FUNC 5 ------------------------------------- */
-void	buffer_write(t_buffer *buffer, int *bytes_read, int fd)
+void	buffer_write(t_buffer *buffer, int *bytes_read, int fd, int *newly_created)
 {
-	if ((buffer->read_head == 0 && buffer->write_head > 0) ||
-		buffer->read_head == buffer->write_head)
+	// if ((buffer->read_head == 0 && buffer->write_head > 0) ||
+	// 	buffer->read_head >= buffer->write_head)
+	if (*newly_created == __ON__ ||
+		buffer->read_head >= buffer->write_head)
 	{
+		//ft_bzero(buffer->chars, BUFFER_SIZE + 1);
 		*bytes_read = read(fd, buffer->chars, BUFFER_SIZE);
 		buffer->read_head = 0;
+		//buffer->write_head = ft_strlen(buffer->chars);
+		buffer->write_head = *bytes_read;
+		buffer->chars[*bytes_read] = '\0';
 	}
 }
 /* ----------------------------- FUNC 6 ------------------------------------- */
-void	line_write(t_line *line, t_buffer *buffer, int *i)
+void	line_write(t_line *line, t_buffer *buffer)
 {
-	while (buffer->read_head < BUFFER_SIZE)
+	while (buffer->read_head < buffer->write_head)
 	{
 		if (buffer->chars[buffer->read_head] != '\n' && buffer->chars[buffer->read_head] != '\0')
 		{
@@ -100,6 +115,12 @@ void	line_write(t_line *line, t_buffer *buffer, int *i)
 		{
 			line_append_chars(line, buffer->chars[buffer->read_head]);
 			buffer->read_head += 1;
+			line_append_chars(line, '\0');
+			line->filled -= 1;
+			break;
+		}
+		else if (buffer->chars[buffer->read_head] == '\0')
+		{
 			line_append_chars(line, '\0');
 			line->filled -= 1;
 			break;
@@ -121,9 +142,9 @@ void	line_append_chars(t_line *line, char c)
 	{
 		temp = (char *)malloc(sizeof(char) * (line->filled + BUFFER_SIZE + 1));
 		if (temp == NULL)
-		{ // delete later
+		{ // noch loeschen
 			//return (NULL); // muss noch abgefangen werden!!!!!!
-		} // delete later
+		}  // noch loeschen
 		else
 		{
 			j = 0;
@@ -139,7 +160,6 @@ void	line_append_chars(t_line *line, char c)
 	}
 	line->chars[line->filled] = c;
 	line->filled++;
-	//write(1, &line->chars[line->filled - 1], 33); // ########## TPO #############
 }
 /* ----------------------------- FUNC 8 ------------------------------------- */
 void	ft_bzero(void *s, size_t n)
@@ -156,4 +176,13 @@ void	ft_bzero(void *s, size_t n)
 	}
 }
 /* ----------------------------- FUNC 9 ------------------------------------- */
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
 /* ----------------------------- FUNC 10 ------------------------------------ */
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: alanghan <alanghan@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/21 10:23:01 by alanghan          #+#    #+#             */
-/*   Updated: 2021/09/01 11:53:41 by alanghan         ###   ########.fr       */
+/*   Updated: 2021/09/02 10:37:20 by alanghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@
 # include <unistd.h>
 
 /* -------------------------- DEFINES --------------------------------------- */
-# define __NEWLINE__ -2
+# define __NEWLINE__ -2 // needed??? not found in code!
+# define __OFF__ 0
+# define __ON__ 1
 
 typedef struct s_buffer
 {
-	char	chars[BUFFER_SIZE];
+	char	chars[BUFFER_SIZE + 1];
 	int		write_head;
 	int		read_head;
 }					t_buffer;
@@ -37,12 +39,13 @@ typedef struct s_line
 
 /* -------------------------- PROTOTYPES ------------------------------------ */
 char	*get_next_line(int fd);
-void	buffer_create(t_buffer *buffer);
-void	line_create(t_line *line);
+void	buffer_create(t_buffer *buffer, int *newly_created);
+int		line_create(t_line *line);
 void	buffer_destroy(t_buffer *buffer);
-void	buffer_write(t_buffer *buffer, int *bytes_read, int fd);
-void	line_write(t_line *line, t_buffer *buffer, int *i);
+void	buffer_write(t_buffer *buffer, int *bytes_read, int fd, int *newly_created);
+void	line_write(t_line *line, t_buffer *buffer);
 void	line_append_chars(t_line *line, char c);
 void	ft_bzero(void *s, size_t n);
+size_t	ft_strlen(const char *s);
 
 #endif
